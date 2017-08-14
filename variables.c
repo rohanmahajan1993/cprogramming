4017724e9cf09a2ec8c407bfbd534aef5394d125
Rohan Mahajan <you@example.com>
2017-08-14 13:59:22 -0700

    one

--- variables.c
+++ variables.c
@@ -1,8 +1,30 @@
 #include <assert.h>
+#include <stdlib.h>
+#include <string.h>
+// These values are stored in the data and bss sections
+// They are always initialized to 0; because easy for system to do
+int value_global; // This is a global variable and can be referenced from other files
+static int value_static; // This variable can only be referenced from this file and other files can use same name for static variables
+ 
 
-int value; // This is a global variable and gets its own space in the executable
+void heap_allocator() {
+  int * malloc_ptr = (int *)malloc(sizeof(int));
+  //malloc does not zero out data; you get whatever was there before and so have to use memset
+  memset(malloc_ptr, 0, sizeof(int)); // 0 is value to set every byte; sizeofint is number of bytes
+  assert(*malloc_ptr == 0);
+  *malloc_ptr = 5;
+  assert(*malloc_ptr == 5);
+  // calloc takes two parameter versus one for malloc due to historical reasons
+  int * calloc_ptr = (int *)calloc(1, sizeof(int));
+  assert(*calloc_ptr == 0);
+}
 
 int main() {
-  value = 5;
-  assert(value == 5);
+  value_global = 5;
+  value_static = 3;
+  int value_stack; // This variables is declared and allocated on the stack; initiaialized to whatever was present before
+  assert(value_global == 5);
+  assert(value_static == 3);
+  heap_allocator();
 }
+
