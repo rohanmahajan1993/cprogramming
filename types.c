c701234822fc0867a4ea74ea1967a1c348e87a24
Rohan Mahajan <you@example.com>
2017-08-14 22:24:44 -0700

    enums

--- types.c
+++ types.c
@@ -1,7 +1,16 @@
-char ch = 15; \\1 bytes
-unsigned char c = 25; \\ 1 byte only positive
-int i = 25; \\ 2 to 4 bytes
-long  = 15; \\has more bytes than int
-float f = 3.0 \\decimal value
-double d = 5.0 \\has more bytes than float and thus has higher range
-const double f = 5.0; \\const just lets compiler know value won't change; can impact optimizations, checking, and make it clearer for programmer 
+#include <assert.h>
+
+char ch = 15; //1 bytes
+unsigned char c = 25; // 1 byte only positive
+int i = 25; // 2 to 4 bytes
+long l  = 15; //has more bytes than int
+float f = 3.0; //decimal value
+double d = 5.0; //has more bytes than float and thus has higher range
+const double constf = 5.0; //const just lets compiler know value won't change; can impact optimizations, checking, and make it clearer for programmer 
+
+int main() {
+    enum week{Mon, Tue, Wed, Thur, Fri, Sat, Sun};
+    enum week day;
+    day = Wed;
+    assert(day == Wed);
+}
