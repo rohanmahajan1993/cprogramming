5ba5ed4bf90c013a5c1bf62f38d3ac465eae95e6
Rohan Mahajan <you@example.com>
2017-08-15 13:48:37 -0700

    F

--- classes.cpp
+++ classes.cpp
@@ -1,16 +1,13 @@
-class person
-{
-  public:
-   int height;
-   int age;
-};
-
-
-
+// http://www.cplusplus.com/doc/tutorial/classes/
+#include <assert.h>
+#include <classes.h>
 
 int main() 
 {
-  person a, b;
-  a.name = "Calvin"
-  a.age = 30;
+  Rectangle a, b;
+  a.width = 5;
+  a.height = 7;
+  //this line would fail because private variable a.color = 4;
+  assert(a.width == 5);  
+  assert(a.height == 7);  
 }
---
+++ classes.h
@@ -0,0 +1,8 @@
+// Generally wwant to declare structures here but have implementations here
+class Rectangle
+{
+ int color; // private variables
+ public:
+   int height;
+   int width;
+};
