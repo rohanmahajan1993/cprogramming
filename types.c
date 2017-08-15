0b0e679ddba2a8fbad53c8b7c0b82160047b87c7
Rohan Mahajan <you@example.com>
2017-08-14 17:10:51 -0700

    types.c

---
+++ types.c
@@ -0,0 +1,7 @@
+char ch = 15; \\1 bytes
+unsigned char c = 25; \\ 1 byte only positive
+int i = 25; \\ 2 to 4 bytes
+long  = 15; \\has more bytes than int
+float f = 3.0 \\decimal value
+double d = 5.0 \\has more bytes than float and thus has higher range
+const double f = 5.0; \\const just lets compiler know value won't change; can impact optimizations, checking, and make it clearer for programmer 
